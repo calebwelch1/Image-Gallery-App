65e52a527d3f8465e0ae90eeb522a513d1ea74bf
Steffi Joel <steffijerome0809>
2020-05-11 20:27:56 -0400

    adding unsplashhtml updated, unsplashscript.js updated

--- Unsplashscript.js
+++ Unsplashscript.js
@@ -1,8 +1,15 @@
+// QUERYING DATA BASED ON A GIVEN SEARCH GENRE
+
+$(document).ready(function () {
+  $(".dropdown-trigger").dropdown();
+});
+
 function searchPhotos() {
-  let search = document.getElementById("search").value; // to search a list of photos based on query
+  let search = document.getElementById("search").value;
+  // to search a list of photos based on query
   var apiKey = "HW9N_DcouND3LdAQFb-NUM_s-4BDQtFZ4R7_JANDOaM";
   var url =
-    "https://api.unsplash.com/collections?page=2&client_id=" +
+    "https://api.unsplash.com/search/photos/?page=5&client_id=" +
     apiKey +
     "&query=" +
     search;
@@ -10,14 +17,32 @@ function searchPhotos() {
     url: url,
     method: "GET",
   }).then(function (response) {
-    console.log(response);
-    response.forEach((photo) => {
-      var result = `<img src="${photo.preview_photos.urls.regular}" class="image">
-         <a href="${photo.links.download}">`;
-      $("#result").append(result);
+    var image1 = response.results[0].urls.regular;
+    console.log(image1);
 
-      // document.getElementById("result").append(result);
-      //console.log(response[0].preview_photos[0].urls.regular);
-    });
+    $("#list")
+      .children(".item-image1")
+      .html(
+        $("<img>", {
+          src: image1,
+          alt: "image1Icon",
+          width: "350px",
+          height: "200px",
+        })
+      );
+
+    var image2 = response.results[1].urls.regular;
+    $("#list")
+      .children(".item-image2")
+      .html(
+        $("<img>", {
+          src: image2,
+          alt: "image2Icon",
+          width: "350px",
+        })
+      );
+
+    $("#result").append(result);
+    // document.getElementById("result").append(result);
   });
 }
